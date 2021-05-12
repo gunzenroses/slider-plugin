d6ee954617b9e8f4bcb03a3b15ca5a9ce0771d9b
Karina G <coren.gun@gmail.com>
2021-05-12 23:30:51 +0800

    add: imports of dependancies and call of SliderMaker function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+import "../src/assets/styles/slider.scss"
+import { SliderMaker } from "./assets/scripts/sliderMaker"
+
 window.onload = function() {
-    console.log("heeey")
+    console.log("heeey");
+    const $ = require('jquery')
+    $("div").toggleClass("active");
+
+    // здесь вызывать rangeSlider.ts for every slider
+    //$.fn.SliderMaker()
+    
+    let simpleSlider = SliderMaker("RS", {
+        max: 200,
+    })
 }
