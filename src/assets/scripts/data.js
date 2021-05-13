1dc8bd1f545db5fefd9c798a2e7d30f12eab67ae
Karina G <coren.gun@gmail.com>
2021-05-13 20:23:10 +0800

    feat: uncomment 'range' key

--- src/assets/scripts/data.js
+++ src/assets/scripts/data.js
@@ -1,8 +1,8 @@
 let sliderData = {
     min: 0,
-    max: 100,
-    //range: false, +progressBar
-    currentLeft: 0,
+    max: 400,
+    range: false,
+    currentFirst: 0,
     //currentRight: 50,
     step: 1,
     //orientation: horizontal,
