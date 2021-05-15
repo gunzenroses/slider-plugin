12fa819651c980fb60b779198259bef224c0a40b
Karina G <coren.gun@gmail.com>
2021-05-15 16:42:48 +0800

    CHANGE: add classes 'rangeFalse' and 'rangeTrue' to render the view

---
+++ src/assets/scripts/strateries/rangeFalse.js
@@ -0,0 +1,20 @@
+import { SliderContainer } from "../subviews/SliderContainer"
+import { SliderTrack } from "../subviews/SliderTrack"
+import { SliderThumb } from "../subviews/SliderThumb"
+import { SliderRange } from "../subviews/SliderRange"
+
+class RangeFalse {
+    constructor(context, containerId){
+        context.sliderContainer = new SliderContainer(containerId);
+        context.sliderTrack = new SliderTrack();
+            context.sliderContainer.prepend(context.sliderTrack);
+        context.sliderThumb = new SliderThumb("thumb_first")
+            context.sliderContainer.append(context.sliderThumb);
+        context.sliderRange = new SliderRange("slider__range");
+            context.sliderContainer.append(context.sliderRange);
+        console.log("falseee")
+        return context;
+    }
+}
+
+export { RangeFalse }
