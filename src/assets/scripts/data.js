69d2a9fb0bfc300a36aed8a1f89dfcfac2e2cb15
Karina G <coren.gun@gmail.com>
2021-05-15 16:41:12 +0800

    feat: update data for range slider

--- src/assets/scripts/data.js
+++ src/assets/scripts/data.js
@@ -1,9 +1,9 @@
 let sliderData = {
     min: 0,
     max: 400,
-    range: false,
-    currentFirst: 0,
-    //currentRight: 50,
+    range: true,
+    currentFirst: 33,
+    currentSecond: 66,
     step: 1,
     //orientation: horizontal,
     //label: false,
