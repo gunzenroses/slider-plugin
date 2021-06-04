20ba48f4d93d134cdbcbca33899d5c201ff50438
Karina G <coren.gun@gmail.com>
2021-06-04 19:58:10 +0800

    CHANGE: rewrite sliderData in Typescripts

---
+++ src/assets/scripts/data.ts
@@ -0,0 +1,15 @@
+import { Settings } from "./types/types"
+
+let sliderData: Settings = {
+    min: 0,
+    max: 400,
+    range: true,
+    currentFirst: 33,
+    currentSecond: 66,
+    step: 1,
+    //orientation: horizontal,
+    //label: false,
+    //scale: false,
+}
+
+export { sliderData }
