d4e7efda04383eed43b7c5e7be1e43af7bdeeffb
Karina G <coren.gun@gmail.com>
2021-06-04 19:59:08 +0800

    CHANGE: rewrite SliderMaker in TypeScript

---
+++ src/assets/scripts/sliderMaker.ts
@@ -0,0 +1,25 @@
+import { sliderData } from "./data"
+import { Settings } from "./types/types"
+import { SliderModel } from "./model"
+import { SliderView } from "./view"
+import { SliderPresenter } from "./presenter"
+
+function SliderMaker(id: string, options: Settings){
+
+    let settings = function (sliderData: Settings, options: Settings){
+        var c: Settings = {};
+        let key: string;
+        for (key in sliderData){
+            if (sliderData.hasOwnProperty(key)){
+                c[key] = key in options ? options[key] : sliderData[key]
+            }
+        };
+        return c;
+    }
+
+    let aModel = new SliderModel(id, settings(sliderData,options));
+    let aView = new SliderView(id);
+    let aPresenter = new SliderPresenter(aModel, aView);
+}
+
+export { SliderMaker }
