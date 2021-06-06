5592be234f4cfbc9fbce6ee029cf1c41ca33c6aa
Karina G <coren.gun@gmail.com>
2021-06-06 20:45:48 +0800

    delete: comment with info about previous logic for sliderRangeClass; add: className for single-thumbed horizontal slider

--- src/assets/scripts/subview/trackView/sliderRange/sliderRangeView.ts
+++ src/assets/scripts/subview/trackView/sliderRange/sliderRangeView.ts
@@ -2,16 +2,11 @@ function sliderRangeView(ifRange: boolean, ifHorizontal: boolean){
     let sliderRangeClass: string = ifRange
                             ? (ifHorizontal ? "slider__range_true"
                                             : "slider__range_vertical-true")
-                            : (ifHorizontal ? ""
+                            : (ifHorizontal ? "slider__range"
                                             : "slider__range_vertical")
-    // let sliderRangeClass: string = ifHorizontal
-    //                         ? ( ifRange ? "slider__range_true" 
-    //                                     : "")
-    //                         : ( ifRange ? "slider__range_vertical-true" 
-    //                                     : "slider__range_vertical");
     return (
         `
-        <div class="slider__range ${sliderRangeClass}"></div>
+        <div class="${sliderRangeClass}"></div>
         `
     )
 }
