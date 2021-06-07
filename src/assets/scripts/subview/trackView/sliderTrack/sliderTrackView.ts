52a2ffce5ef167e01f14b6748e5e4e0e9b69d5e1
Karina G <coren.gun@gmail.com>
2021-06-07 15:54:01 +0800

    feat: return sliderTrack not as a string, but as an HTMLElement with help of DOM manipulation

--- src/assets/scripts/subview/trackView/sliderTrack/sliderTrackView.ts
+++ src/assets/scripts/subview/trackView/sliderTrack/sliderTrackView.ts
@@ -1,18 +1,11 @@
-import { sliderThumbView } from "../sliderThumb/sliderThumbView"
-import { sliderRangeView } from "../sliderRange/sliderRangeView"
-
-function sliderTrackView(ifRange: boolean, ifHorizontal: boolean){
+function sliderTrackView(parentNode: HTMLElement, ifHorizontal: boolean){
     let sliderTrackClass: string = ifHorizontal 
                                     ? "slider__track" 
                                     : "slider__track_vertical" ;
-    return (
-        `
-            <div class=${sliderTrackClass}>
-                ${sliderThumbView(ifRange, ifHorizontal)} 
-                ${sliderRangeView(ifRange, ifHorizontal)}
-            </div>
-        `
-    )
+    let sliderTrack = document.createElement("div");
+    sliderTrack.classList.add(`${sliderTrackClass}`)
+    parentNode.append(sliderTrack);
+    return sliderTrack;
 }
 
 export { sliderTrackView }
