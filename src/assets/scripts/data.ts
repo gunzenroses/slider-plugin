7ba77da01d08bdc93a3b9eb6b3a8af07453d0be3
Karina G <coren.gun@gmail.com>
2021-06-07 15:38:50 +0800

    feat: unlock 'tooltip' option

--- src/assets/scripts/data.ts
+++ src/assets/scripts/data.ts
@@ -2,13 +2,13 @@ import { TSettings } from "./types/types"
 
 let sliderData: TSettings = {
     min: 0,
-    max: 400,
+    max: 100,
     range: true,
     currentFirst: 33,
     currentSecond: 66,
     step: 1,
     orientation: "horizontal",
-    label: true,
+    tooltip: true,
     //step: false
     //scale: false,
 }
