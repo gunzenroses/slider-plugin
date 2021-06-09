3023e0e35411853ce82b363394cced878f481b0e
Karina G <coren.gun@gmail.com>
2021-06-09 13:23:13 +0800

    add: function scaleMaker and two styles for scaleItems; function scaleMaker returns in dependance with stepValue and number of element in a row: - regular style for point - or segment style for point

--- src/assets/scripts/subview/scaleView/scaleItemView.ts
+++ src/assets/scripts/subview/scaleView/scaleItemView.ts
@@ -11,27 +11,32 @@ function scaleItemView(ifHorizontal: boolean, scaleLength: number, min: number, 
     let pointWidth: string = ifHorizontal ? "width" : "height";
     let height: number = heightValue ? heightValue : 3;
     let width: number = widthValue ? widthValue : 1;
-    let pointStyle = `${pointHeight}: ${height}px; ${pointWidth}: ${width}px;`
+    let pointStyle: string = `${pointHeight}: ${height}px; ${pointWidth}: ${width}px;`
+    let pointSegmentStyle: string = `${pointHeight}: ${height*2}px; ${pointWidth}: ${width}px;`
 
     let itemClass: string = ifHorizontal ? "scale__point" : "scale__point_vertical";
     let borderType: string = ifHorizontal ? "left" : "top";
     let borderWidth: number = 1;
     let borderStyle = `border-${borderType}: ${borderWidth}px solid black;`;
 
-    // function scaleMaker(item: number){
-    //     return (
-    //         (item%10 === 0) 
-    //             ? ``
-    //             : "<div class=${itemClass} style="${borderStyle}"></div>"
-    //     )
-    // }
+    function scaleMaker(item: number){
+        return (
+            (item % (10*step) === 0) 
+                ? `<div class=${itemClass} style="${borderStyle} ${pointSegmentStyle}""></div>`
+                : `<div class=${itemClass} style="${borderStyle} ${pointStyle}""></div>`
+        )
+    }
 
     let scaleItemView = 
-            scaleItemRow.map(function(item){
-                return (
-                `<div class=${itemClass} style="${borderStyle} ${pointStyle}"></div>`
-                )
-            }).join(" ");
+            scaleItemRow.map(item => scaleMaker(item)).join(" ");
+
+    // let scaleItemView = 
+        // scaleItemRow.map(function(item){
+        //     return (
+        //     `<div class=${itemClass} style="${borderStyle} ${pointStyle}"></div>`
+        //     )
+        // }).join(" ");
+
     return scaleItemView;
 }
 
