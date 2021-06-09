b1b0e9cac812b07fe48db2bfb539ca7ffa5e6d7a
Karina G <coren.gun@gmail.com>
2021-06-09 20:21:01 +0800

    add: use 'applyStep' function which returns right value for tooltip innerText

--- src/assets/scripts/subview/tooltipView/tooltipItemView.ts
+++ src/assets/scripts/subview/tooltipView/tooltipItemView.ts
@@ -1,8 +1,11 @@
-function tooltipItemView(parentNode: HTMLElement, ifHorizontal: boolean, className: string, value: number): HTMLElement {
+import { applyStep } from "../../common"
+
+function tooltipItemView(parentNode: HTMLElement, ifHorizontal: boolean, className: string, value: number, max: number, step: number): HTMLElement {
     let verticalClass = ifHorizontal ? "" : "-vertical";
     let tooltip = document.createElement("span");
     tooltip.classList.add("tooltip", `${className}${verticalClass}`);
-    tooltip.innerHTML = value.toString();
+    let newValue = Math.round(applyStep(value, max, step));
+    tooltip.innerHTML = newValue.toString();
     parentNode.append(tooltip);
     return tooltip;
 }
