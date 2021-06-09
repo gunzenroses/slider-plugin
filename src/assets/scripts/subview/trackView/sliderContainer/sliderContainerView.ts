d176450a034fc9dd717b8ab2cf005844c0823b9d
Karina G <coren.gun@gmail.com>
2021-06-09 14:25:30 +0800

    init: function for sliderContainer subview

---
+++ src/assets/scripts/subview/trackView/sliderContainer/sliderContainerView.ts
@@ -0,0 +1,11 @@
+function sliderContainerView(parentNode: HTMLElement, ifHorizontal: boolean){
+    let sliderContainerClass = ifHorizontal
+                        ? "slider__content" 
+                        : "slider__content_vertical";
+    let sliderContainer = document.createElement("div");
+    sliderContainer.classList.add(sliderContainerClass);
+    parentNode.append(sliderContainer);
+    return sliderContainer;
+}
+
+export { sliderContainerView }
