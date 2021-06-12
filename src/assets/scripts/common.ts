f661e9d51c4d55c6f586cc4093cdbd9d620f71da
Karina G <coren.gun@gmail.com>
2021-06-13 00:30:16 +0800

    fix: algorythm that compares the remaining after division; mistake was that operator '%' returns number left after comma(,XX)

--- src/assets/scripts/common.ts
+++ src/assets/scripts/common.ts
@@ -1,7 +1,7 @@
 function applyStep(value: number, max: number, step: number): number {
     let stepInPercents: number = parseFloat((100/max * step).toFixed(2));
     let numberOfSteps = value / stepInPercents;
-    let realNumberOfSteps = (value % stepInPercents > 5)
+    let realNumberOfSteps = (value % stepInPercents > stepInPercents/2)
                 ? Math.ceil(numberOfSteps)
                 : Math.floor(numberOfSteps);
     let realValue = (value === 100)
