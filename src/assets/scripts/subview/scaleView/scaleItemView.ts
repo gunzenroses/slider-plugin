6a5b0e1c016a3578e232c81f8a4be3e1b63cd8e9
Karina G <coren.gun@gmail.com>
2021-06-12 19:24:30 +0800

    add: style for margins and special style for max value of scale items

--- src/assets/scripts/subview/scaleView/scaleItemView.ts
+++ src/assets/scripts/subview/scaleView/scaleItemView.ts
@@ -5,38 +5,38 @@ function scaleItemView(ifHorizontal: boolean, scaleLength: number, min: number, 
             scaleItemRow.push(i);
         }
     }
-    scaleItemRow.push(max);
     
     let pointHeight: string = ifHorizontal ? "height" : "width";
     let pointWidth: string = ifHorizontal ? "width" : "height";
-    let height: number = heightValue ? heightValue : 3;
-    let width: number = widthValue ? widthValue : 1;
-    let pointStyle: string = `${pointHeight}: ${height}px; ${pointWidth}: ${width}px;`
-    let pointSegmentStyle: string = `${pointHeight}: ${height*2}px; ${pointWidth}: ${width}px;`
+    let pointHeightValue: number = heightValue ? heightValue : 3;
+    let pointWidthValue: number = widthValue ? widthValue : 1;
+    let pointStyle: string = `${pointHeight}: ${pointHeightValue}px; ${pointWidth}: ${pointWidthValue}px;`
+    let pointSegmentStyle: string = `${pointHeight}: ${pointHeightValue*2}px; ${pointWidth}: ${pointWidthValue}px;`
 
     let itemClass: string = ifHorizontal ? "scale__point" : "scale__point_vertical";
     let borderType: string = ifHorizontal ? "left" : "top";
     let borderWidth: number = 1;
-    let borderStyle: string = `border-${borderType}: ${borderWidth}px solid black;`;
+    let borderStyle: string = "" //`border-${borderType}: ${borderWidth}px solid black;`;
     let spanClass: string = ifHorizontal ? "scale__number" : "scale__number_vertical"
+
+    let marginType: string = ifHorizontal ? "right" : "bottom";
+    let marginWidth: number = scaleLength / scaleItemRow.length - borderWidth;
+    let marginStyle: string = `margin-${marginType}: ${marginWidth}px;`;
+
+    let maxStyle: string = `position: absolute; ${marginType}: 0;`;
+    let scaleItemMax: string = `<div class=${itemClass} style="${maxStyle} ${borderStyle} ${pointSegmentStyle}""><span class="${spanClass}">${max}</span></div>`
 
     function scaleMaker(item: number){
         return (
             (item % (10*step) === 0) 
-                ? `<div class=${itemClass} style="${borderStyle} ${pointSegmentStyle}""><span class="${spanClass}">${item}</span></div>`
-                : `<div class=${itemClass} style="${borderStyle} ${pointStyle}""></div>`
+                ? `<div class=${itemClass} style="${marginStyle} ${borderStyle} ${pointSegmentStyle}""><span class="${spanClass}">${item}</span></div>`
+                : `<div class=${itemClass} style="${marginStyle} ${borderStyle} ${pointStyle}""></div>`
         )
     }
 
     let scaleItemView = 
-            scaleItemRow.map(item => scaleMaker(item)).join(" ");
-
-    // let scaleItemView = 
-        // scaleItemRow.map(function(item){
-        //     return (
-        //     `<div class=${itemClass} style="${borderStyle} ${pointStyle}"></div>`
-        //     )
-        // }).join(" ");
+            scaleItemRow.map(item => scaleMaker(item)).join(" ")
+            + scaleItemMax;
 
     return scaleItemView;
 }
