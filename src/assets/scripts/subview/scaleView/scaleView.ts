9137a767d5dca0b364e97eca46af13d1dbd3605c
Karina G <coren.gun@gmail.com>
2021-06-12 19:17:50 +0800

    add: variable 'parentNodeStyle' so code looks prettier

--- src/assets/scripts/subview/scaleView/scaleView.ts
+++ src/assets/scripts/subview/scaleView/scaleView.ts
@@ -1,23 +1,23 @@
 import { scaleItemView } from "./scaleItemView"
+
 function scaleView(parentNode: HTMLElement, ifHorizontal: boolean, min: number, max: number, stepValue?: number): HTMLElement {
     let scaleClass: string = ifHorizontal
                 ? "slider__scale"
                 : "slider__scale_vertical";
     let step: number;
-    console.log(stepValue)
     stepValue
             ? step = stepValue 
             : step = 1;
     let scale: HTMLElement = document.createElement("div");
     scale.classList.add(scaleClass);
-
+    
+    let parentNodeStyle = getComputedStyle(parentNode);
     let scaleLength = ifHorizontal
-                    ? Math.ceil(parseFloat(getComputedStyle(parentNode).width.replace("px","")))
-                    : Math.ceil(parseFloat(getComputedStyle(parentNode).height.replace("px","")));
+                    ? Math.ceil(parseFloat(parentNodeStyle.width))
+                    : Math.ceil(parseFloat(parentNodeStyle.height));
 
     scale.innerHTML = scaleItemView(ifHorizontal, scaleLength, min, max, step);
     
-
     parentNode.append(scale);
     return scale;
 }
