71abb449e212accd0f6d693bf222768b856fc0e6
Karina G <coren.gun@gmail.com>
2021-06-12 19:09:45 +0800

    feat: rounds returning value with 'Math.ceil' and 'Math.floor' chooses to round returning value up or down, based on value on remainder of the division (value/stepInPercents)

--- src/assets/scripts/common.ts
+++ src/assets/scripts/common.ts
@@ -1,10 +1,13 @@
 function applyStep(value: number, max: number, step: number): number {
     let stepInPercents: number = parseFloat((100/max * step).toFixed(2));
-    let numberOfSteps = Math.round(value / stepInPercents);
-    let newValue = (value === 100)
+    let numberOfSteps = value / stepInPercents;
+    let realNumberOfSteps = (value % stepInPercents > 5)
+                ? Math.ceil(numberOfSteps)
+                : Math.floor(numberOfSteps);
+    let realValue = (value === 100)
                     ? value
-                    : numberOfSteps * stepInPercents;
-    return newValue;
+                    : realNumberOfSteps * stepInPercents;
+    return realValue;
 }
 
 export { applyStep }
