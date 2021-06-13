d14fff62a5cf6de25dd50575005e112f6ece7a5a
Karina G <coren.gun@gmail.com>
2021-06-13 15:34:27 +0800

    add: function 'findPosition'

--- src/assets/scripts/common.ts
+++ src/assets/scripts/common.ts
@@ -14,4 +14,18 @@ function fromPercentsToValue(value: number, max: number, min: number){
     let newValue = Math.round(value * (max - min) / 100).toString();
     return newValue;
 }
-export { applyStep, fromPercentsToValue }
+
+function findPosition(thisElement: HTMLElement, ifHorizontal: boolean, containerSize: number){
+    let newPosition = ifHorizontal
+            ? (thisElement.style.left)
+                ? parseInt(thisElement.style.left.replace("%",""))
+                : parseInt(getComputedStyle(thisElement).left.replace("px",""))/containerSize*100
+            : (thisElement.style.bottom)
+                ? parseInt(thisElement.style.bottom.replace("%",""))
+                : parseInt(getComputedStyle(thisElement).bottom.replace("px",""))/containerSize*100;
+    return newPosition;
+}
+
+
+
+export { applyStep, fromPercentsToValue, findPosition }
