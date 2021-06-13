bc2f720d5066a13ab4c6ab923c26da911b85054b
Karina G <coren.gun@gmail.com>
2021-06-13 15:01:36 +0800

    add: function 'fromPercentsToValue' to which finds a value for tooltip innerText

--- src/assets/scripts/subview/tooltipView/tooltipItemView.ts
+++ src/assets/scripts/subview/tooltipView/tooltipItemView.ts
@@ -1,13 +1,16 @@
-import { applyStep } from "../../common"
+import { applyStep, fromPercentsToValue } from "../../common"
 
-function tooltipItemView(parentNode: HTMLElement, ifHorizontal: boolean, className: string, value: number, max: number, step: number): HTMLElement {
+function tooltipItemView(parentNode: HTMLElement, ifHorizontal: boolean, className: string, value: number, max: number, min: number, step: number): HTMLElement {
     let verticalClass = ifHorizontal ? "" : "-vertical";
     let tooltip = document.createElement("span");
     tooltip.classList.add("tooltip", `${className}${verticalClass}`);
+    
     let newValue = Math.round(applyStep(value, max, step)) > max
                     ? max
                     : Math.round(applyStep(value, max, step));
-    tooltip.innerText = newValue.toString();
+
+    tooltip.innerText = fromPercentsToValue(newValue, max, min);
+    
     parentNode.append(tooltip);
     return tooltip;
 }
