56eec11855cbee393800fbc849f4ad2e465ff663
Karina G <coren.gun@gmail.com>
2021-06-13 17:44:13 +0800

    add: changeRange function

--- src/assets/scripts/subview/trackView/sliderRange/sliderRangeView.ts
+++ src/assets/scripts/subview/trackView/sliderRange/sliderRangeView.ts
@@ -10,4 +10,19 @@ function sliderRangeView(parentNode: HTMLElement, ifRange: boolean, ifHorizontal
     return sliderRange;
 }
 
-export { sliderRangeView }
+function changeRange(object: HTMLElement, newThumbCurrent: number, ifHorizontal: boolean, ifThumbFirst: boolean, ifFirst: boolean){
+    ifThumbFirst
+        ? ifHorizontal
+                ? (ifFirst 
+                    ? object.style.left = newThumbCurrent + "%"
+                    : object.style.right = (100 - newThumbCurrent) + "%")
+                : (ifFirst 
+                    ? object.style.bottom = newThumbCurrent + "%"
+                    : object.style.top = (100 - newThumbCurrent) + "%")
+        : ifHorizontal
+                ? object.style.right = (100 - newThumbCurrent) + "%"
+                : object.style.top = (100 - newThumbCurrent) + "%";
+    return object;
+}
+
+export { sliderRangeView, changeRange }
