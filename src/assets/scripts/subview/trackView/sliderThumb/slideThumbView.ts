53a3ac17c2a39ff3a207b397f66c802530cb9614
Karina G <coren.gun@gmail.com>
2021-06-14 12:32:40 +0800

    fix: add style for thumb with help of 'applyStep', 'applyRestrictions', 'changeThumb'. left/bottom position of thumb takes into accounts 'step' and 'max' values.

--- src/assets/scripts/subview/trackView/sliderThumb/slideThumbView.ts
+++ src/assets/scripts/subview/trackView/sliderThumb/slideThumbView.ts
@@ -1,15 +1,24 @@
-function sliderThumbView(parentNode: HTMLElement, className: string, ifHorizontal: boolean) {
+import { applyStep, applyRestrictions } from "../../../common"
+import { changeThumb } from "./changeThumb"
+
+function sliderThumbView(parentNode: HTMLElement, className: string, ifHorizontal: boolean, max: number, step: number) {
     let verticalClass = ifHorizontal ? "" : "-vertical"
     let thumbClass = `${className}${verticalClass}`
     let sliderThumbView = document.createElement("div");
     sliderThumbView.classList.add("slider__thumb", thumbClass);
     
+    let posTemp = (className === "thumb_first")
+                    ? 33
+                    : 66;
+    let posChecked = applyRestrictions(applyStep(posTemp, max, step));
+    // ifHorizontal 
+    //             ? sliderThumbView.style.left = posChecked + "%"
+    //             : sliderThumbView.style.bottom = posChecked + "%";
+
+    changeThumb(sliderThumbView, ifHorizontal, posChecked)
+
     parentNode.append(sliderThumbView);
     return sliderThumbView;
-
-    //findPosition
-    //applyStep(newThumbValue, max, step);
-    //changeThumb(object: any, ifHorizontal: boolean, newThumbCurrent: number)
 }
 
 export { sliderThumbView };
