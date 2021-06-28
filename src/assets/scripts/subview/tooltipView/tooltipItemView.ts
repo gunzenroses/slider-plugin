54cda5f469046d39d6b0bc502090b72361312126
Karina G <coren.gun@gmail.com>
2021-06-28 19:10:13 +0800

    feat: change order of parameters

--- src/assets/scripts/subview/tooltipView/tooltipItemView.ts
+++ src/assets/scripts/subview/tooltipView/tooltipItemView.ts
@@ -1,6 +1,6 @@
 import { applyStep, fromPercentsToValue } from "../../common"
 
-function tooltipItemView(parentNode: HTMLElement, ifHorizontal: boolean, className: string, value: number, max: number, min: number, step: number): HTMLElement {
+function tooltipItemView(parentNode: HTMLElement, className: string, value: number, ifHorizontal: boolean, max: number, min: number, step: number): HTMLElement {
     let verticalClass = ifHorizontal ? "" : "-vertical";
     let tooltip = document.createElement("span");
     tooltip.classList.add("tooltip", `${className}${verticalClass}`);
