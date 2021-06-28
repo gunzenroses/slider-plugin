018517c179a7ff305b3ec318ad08c6c43381afbc
Karina G <coren.gun@gmail.com>
2021-06-28 14:58:37 +0800

    feat: rename 'specialNum' -> 'sequenceNum' and add comments

--- src/assets/scripts/subview/scaleView/scaleItemRow.ts
+++ src/assets/scripts/subview/scaleView/scaleItemRow.ts
@@ -2,34 +2,41 @@ import { fromValueToPX } from "../../common"
 import { scaleItem } from "./scaleItem"
 
 function scaleItemRow (ifHorizontal: boolean, containerSize: number, min: number, max: number, step: number, heightValue?: number, widthValue?: number){
+    //find the row of elements (with step)
     let scaleItemRow: number[] = [];
     let i=min;
     while (i<max){
         scaleItemRow.push(i);
         i+=step;
     }
+
+    //find the sequence number of the segment 
+    let sequenceNum = (scaleItemRow.length+1) % 2 === 0
+                        ? 2
+                        : 3;
+
+    //determine classes for elements
     let segmentClass: string = ifHorizontal ? "scale__segment" : "scale__segment_vertical";
     let spanClass: string = ifHorizontal ? "scale__number" : "scale__number_vertical";
+    let scaleItemClass = ifHorizontal ? "scale__row" : "scale__row_vertical";
 
+    //to find the length of row of divisions (exc the max value)
     let lengthOfLeft = max - scaleItemRow[scaleItemRow.length-1];
     let newContainerSize = containerSize - fromValueToPX(lengthOfLeft, max, min, containerSize) - 1;
 
+    //creation of row of items
     let scaleItems: HTMLElement = document.createElement("div");
-    let scaleItemClass = ifHorizontal ? "scale__row" : "scale__row_vertical"
     scaleItems.classList.add(scaleItemClass);
     ifHorizontal
         ? scaleItems.style.width = newContainerSize + "px"
         : scaleItems.style.height = newContainerSize + "px";
 
+    //special style for item with max value
     let marginType: string = ifHorizontal ? "right" : "top";
     let maxStyle: string = `position: absolute; ${marginType}: 0;`;
     let scaleItemMax: string = `<div class=${segmentClass} style="${maxStyle}""><span class="${spanClass}">${max}</span></div>`
 
-    let specialNums = (scaleItemRow.length+1) % 2 === 0
-                    ? 2
-                    : 3;
-
-    scaleItems.innerHTML = scaleItemRow.map(item => scaleItem(item, specialNums, ifHorizontal, segmentClass, spanClass, min, max, step)).join(" ") 
+    scaleItems.innerHTML = scaleItemRow.map(item => scaleItem(item, sequenceNum, ifHorizontal, segmentClass, spanClass, min, max, step)).join(" ") 
                         + scaleItemMax;
 
     return scaleItems;
