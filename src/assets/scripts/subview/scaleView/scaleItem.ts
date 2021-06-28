d7d4c5896471b5b59c6a978f883b68de972708c1
Karina G <coren.gun@gmail.com>
2021-06-28 14:56:59 +0800

    rename: 'specialNum' -> 'sequenceNum'

--- src/assets/scripts/subview/scaleView/scaleItem.ts
+++ src/assets/scripts/subview/scaleView/scaleItem.ts
@@ -1,9 +1,9 @@
 
-function scaleItem(item: number, specialNums: number, ifHorizontal: boolean, segmentClass: string, spanClass: string, min: number, max: number, step: number){
+function scaleItem(item: number, sequenceNum: number, ifHorizontal: boolean, segmentClass: string, spanClass: string, min: number, max: number, step: number){
     let itemClass: string = ifHorizontal ? "scale__point" : "scale__point_vertical";
     
     return (
-        (item === min || (item - min) % (specialNums*step) === 0) 
+        (item === min || (item - min) % (sequenceNum*step) === 0) 
             ? `<div class=${segmentClass}><span class="${spanClass}">${item}</span></div>`
             : `<div class=${itemClass}></div>`
     )
