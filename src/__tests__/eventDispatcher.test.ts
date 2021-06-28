7e15c014d0e899111880aa33cac421cc6eb01599
Karina G <coren.gun@gmail.com>
2021-06-28 14:25:39 +0800

    feat: delete comments and rename tests

--- src/__tests__/eventDispatcher.test.ts
+++ src/__tests__/eventDispatcher.test.ts
@@ -1,36 +1,27 @@
 import { EventDispatcher } from "../assets/scripts/eventDispatcher";
 
-describe('eventDispatcher',()=>{
-    //arrange
+describe('EventDispatcher',()=>{
     let ed = new EventDispatcher();
     let listener = function(val: number){ return val+1 };
-    //act
+
     ed.add(listener);
-    
-    describe('function add()', ()=>{
-        test('should add listener to listeners', ()=>{
-            //assert
-            expect(ed.listeners.length).toBe(1);
-        })
+    test('method add(): add listener to listeners', ()=>{
+        //assert
+        expect(ed.listeners.length).toBe(1);
+    })
 
-        test('should pass arguments when it notifies listeners', ()=>{
-            //arrange
-            let message = false;
-            let args = (msg: boolean)=>{ msg = true }
+    test('method notify(): pass arguments when it notifies listeners', ()=>{
+        let message = false;
+        let args = (msg: boolean)=>{ msg = true }
 
-            //act
-            ed.notify(args(message));
+        ed.notify(args(message));
 
-            //assert
-            expect(message).toBeTruthy;
-        })
+        expect(message).toBeTruthy;
+    })
 
-        test('should remove listener from listeners', ()=>{
-            //act
-            ed.remove(listener);
+    test('method remove(): remove listener from listeners', ()=>{
+        ed.remove(listener);
 
-            //assert
-            expect(ed.listeners.length).toBe(0);
-        })
+        expect(ed.listeners.length).toBe(0);
     })
 })
