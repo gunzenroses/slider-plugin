ddbe046ab6ebba379bcc9f33aeb07a45369aba52
Karina G <coren.gun@gmail.com>
2021-07-02 12:44:15 +0800

    feat: move 'sender()' to 'common.ts' as 'mergeData()'

--- src/assets/scripts/sliderMaker.ts
+++ src/assets/scripts/sliderMaker.ts
@@ -5,21 +5,11 @@ import { SliderModel } from "./model"
 import { SliderView } from "./view"
 import { SliderPresenter } from "./presenter"
 import { ConfigurationPanel } from "./panel/panel"
+import { mergeData } from "./common"
 
 function SliderMaker(id: string, options: TSettings, configurationPanel?: boolean){
 
-    let settings = function (sliderData: TSettings, options: TSettings){
-        var c: TSettings = {};
-        let key: string;
-        for (key in sliderData){
-            if (sliderData.hasOwnProperty(key)){
-                c[key] = key in options ? options[key] : sliderData[key]
-            }
-        };
-        return c;
-    }
-
-    let aModel = new SliderModel(id, settings(sliderData,options));
+    let aModel = new SliderModel(id, mergeData(sliderData, options));
     let aView = new SliderView(id);
     let aPresenter = new SliderPresenter(aModel, aView);
 
