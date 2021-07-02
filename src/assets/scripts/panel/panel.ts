7a7190942dda31ac200905859742cff7ed7d554b
Karina G <coren.gun@gmail.com>
2021-07-03 04:32:28 +0800

    add: cach for every element + functionality on case of changes

--- src/assets/scripts/panel/panel.ts
+++ src/assets/scripts/panel/panel.ts
@@ -17,10 +17,30 @@ class ConfigurationPanel implements IPanel {
     panelContainer!: HTMLElement;
     listOfPanelItems!: any;
 
+    minInput!: HTMLInputElement;
+    maxInput!: HTMLInputElement;
+    stepInput!: HTMLInputElement;
+    currentFirstInput!: HTMLInputElement;
+    currentSecondInput!: HTMLInputElement;
+    verticalInput!: HTMLInputElement;
+    rangeInput!: HTMLInputElement;
+    scaleInput!: HTMLInputElement;
+    tooltipInput!: HTMLInputElement;
+
     updateHandler!: {(name: string, value: number): void};
     changeMinHandler!: {(): void};
-    changeMaxHanler!: {(): void};
+    changeMaxHandler!: {(): void};
+    changeStepHandler!: {(): void};
+    changeCurrentFirstHandler!: {(): void};
+    changeCurrentSecondHandler!: {(): void};
+    changeOrientationHandler!: {(): void};
+    changeRangeHandler!: {(): void};
+    changeScaleHandler!: {(): void};
+    changeTooltipHandler!: {(): void};
+
     changePanelHandler!: {(event: Event): void};
+    updateThumbHandler!: {(number: number): void};
+    updateThumbSecondHandler!: {(number: number): void};
 
     constructor(containerId: string, presenter: IPresenter){
         this.parentContainer = document.getElementById(containerId)!;
@@ -34,25 +54,123 @@ class ConfigurationPanel implements IPanel {
 
     init(){
         this.render(this.data);
+        this.createChildren();
         this.setupHandlers();
         this.enable();
     }
 
+    createChildren(){
+        this.minInput = <HTMLInputElement>document.getElementsByName("min")[0];
+        this.maxInput = <HTMLInputElement>document.getElementsByName("max")[0];
+        this.stepInput = <HTMLInputElement>document.getElementsByName("step")[0];
+        this.currentFirstInput = <HTMLInputElement>document.getElementsByName("currentFirst")[0];
+        this.currentSecondInput = <HTMLInputElement>document.getElementsByName("currentSecond")[0];
+        this.verticalInput = <HTMLInputElement>document.getElementsByName("vertical")[0];
+        this.rangeInput = <HTMLInputElement>document.getElementsByName("range")[0];
+        this.scaleInput = <HTMLInputElement>document.getElementsByName("scale")[0];
+        this.tooltipInput = <HTMLInputElement>document.getElementsByName("tooltip")[0];
+    }
+
     setupHandlers(){
-        this.changePanelHandler = this.changePanel.bind(this);
+        //this.changePanelHandler = this.changePanel.bind(this);
+        this.changeMinHandler = this.changeMin.bind(this);
+        this.changeMaxHandler = this.changeMax.bind(this);
+        this.changeStepHandler = this.changeStep.bind(this);
+        this.changeCurrentFirstHandler = this.changeCurrentFirst.bind(this);;
+        this.changeCurrentSecondHandler = this.changeCurrentSecond.bind(this);
+        this.changeOrientationHandler = this.changeOrientation.bind(this);
+        this.changeRangeHandler = this.changeRange.bind(this);
+        this.changeScaleHandler = this.changeScale.bind(this);
+        this.changeTooltipHandler = this.changeTooltip.bind(this);
+
         this.updateHandler = this.updatePanel.bind(this);
+        this.updateThumbHandler = this.updateThumb.bind(this);
+        this.updateThumbSecondHandler = this.updateThumbSecond.bind(this);
     }
 
     enable(){
-        this.panelContainer.addEventListener('change', this.changePanelHandler);
+        //this.panelContainer.addEventListener('change', this.changePanelHandler);
+        this.minInput.addEventListener('change', this.changeMinHandler);
+        this.maxInput.addEventListener('change', this.changeMaxHandler);
+        this.stepInput.addEventListener('change', this.changeStepHandler);
+        this.currentFirstInput.addEventListener('change', this.changeCurrentFirstHandler);
+        this.currentSecondInput.addEventListener('change', this.changeCurrentSecondHandler);
+        this.verticalInput.addEventListener('change', this.changeOrientationHandler);
+        this.rangeInput.addEventListener('change', this.changeRangeHandler);
+        this.scaleInput.addEventListener('change', this.changeScaleHandler);
+        this.tooltipInput.addEventListener('change', this.changeTooltipHandler);
+    
         this.presenter.fromPresenterUpdate.add(this.updateHandler);
+        this.presenter.fromPresenterThumbUpdate.add(this.updateThumbHandler);
+        this.presenter.fromPresenterThumbSecondUpdate.add(this.updateThumbSecondHandler);
     }
 
-    changePanel(e: Event){
-        let element = e.target as HTMLInputElement;
-        let name = element.getAttribute("name")!;
-        let data = parseInt(element.value);
-        this.presenter.setData({ [name]: data });
+    updateThumb(value: number){
+        this.currentFirstInput.value = value.toString();
+    }
+
+    updateThumbSecond(value: number){
+        this.currentSecondInput.value = value.toString();
+    }
+
+    // changePanel(e: Event){
+    //     let element = e.target as HTMLInputElement;
+    //     let name = element.getAttribute("name")!;
+    //     let data = parseInt(element.value);
+    //     this.presenter.setData({ [name]: data });
+    // }
+
+    changeMin(){
+        let data = parseInt(this.minInput.value);
+        this.presenter.setData({min: data});
+    }
+
+    changeMax(){
+        let data = parseInt(this.maxInput.value);
+        this.presenter.setData({max: data});
+    }
+
+    changeStep(){
+        let data = parseInt(this.stepInput.value);
+        this.presenter.setData({step: data});
+    }
+
+    changeCurrentFirst(){
+        let data = parseInt(this.currentFirstInput.value);
+        this.presenter.changeThumbInModel(data);
+    }
+
+    changeCurrentSecond(){
+        let data = parseInt(this.currentSecondInput.value);
+        this.presenter.changeThumbSecondInModel(data);
+    }
+
+    changeOrientation(){
+        let data = (this.verticalInput.checked)
+            ? "vertical"
+            : "horizontal"
+        this.presenter.setData({orientation: data});
+    }
+
+    changeRange(){
+        let data = (this.rangeInput.checked)
+            ? true
+            : false;
+        this.presenter.setData({range: data});
+    }
+
+    changeScale(){
+        let data = (this.scaleInput.checked)
+            ? true
+            : false;
+        this.presenter.setData({scale: data});
+    }
+
+    changeTooltip(){
+        let data = (this.tooltipInput.checked)
+            ? true
+            : false;
+        this.presenter.setData({tooltip: data});
     }
 
     createPanelItem(params: any){
