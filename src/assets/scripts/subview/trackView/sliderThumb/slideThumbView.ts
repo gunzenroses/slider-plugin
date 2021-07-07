6234274d398b2863996313362b859710e7997645
Karina G <coren.gun@gmail.com>
2021-07-07 20:19:39 +0800

    feat: use calculated 'currentNum'(%) instead of applyStep('max', 'min', 'step')

--- src/assets/scripts/subview/trackView/sliderThumb/slideThumbView.ts
+++ src/assets/scripts/subview/trackView/sliderThumb/slideThumbView.ts
@@ -1,18 +1,13 @@
-import { applyStep, applyRestrictions } from "../../../common"
+import { applyStepOnPercents, applyRestrictions } from "../../../common"
 import { changeThumb } from "./changeThumb"
 
-function sliderThumbView(parentNode: HTMLElement, className: string, ifHorizontal: boolean, max: number, min: number, step: number) {
+function sliderThumbView(parentNode: HTMLElement, className: string, ifHorizontal: boolean, currentNum: number) {
     let verticalClass = ifHorizontal ? "" : "-vertical"
     let thumbClass = `${className}${verticalClass}`
     let sliderThumbView = document.createElement("div");
     sliderThumbView.classList.add("slider__thumb", thumbClass);
-    
-    let posTemp = (className === "thumb_first")
-                    ? 33
-                    : 66;
-    let posChecked = applyRestrictions(applyStep(posTemp, max, min, step));
-    changeThumb(sliderThumbView, ifHorizontal, posChecked)
 
+    changeThumb(sliderThumbView, ifHorizontal, currentNum)
     parentNode.append(sliderThumbView);
     return sliderThumbView;
 }
