4f6d2e3d74f252c06d59afc0d193270ab028df75
Karina G <coren.gun@gmail.com>
2021-07-08 00:56:35 +0800

    add: 'select()' case when distance from first and second thumbs to selected place are equal

--- src/assets/scripts/presenter.ts
+++ src/assets/scripts/presenter.ts
@@ -141,12 +141,18 @@ class SliderPresenter implements IPresenter {
         let firstDiff: number = Math.abs(firstThumbPercent - newThumbCurrentPercent);
         let secondDiff: number = Math.abs(secondThumbPercent - newThumbCurrentPercent);
 
-        if (firstDiff < secondDiff){ 
+        if (firstDiff <= secondDiff){ 
             this.view.selectObject = this.view.sliderThumb;
-            this.changeThumbInModel(newThumbCurrentPercent) 
+            this.changeThumbInModel(newThumbCurrentPercent); 
         } if (firstDiff >= secondDiff){
             this.view.selectObject = this.view.sliderThumbSecond!;
             this.changeThumbSecondInModel(newThumbCurrentPercent);
+        } if (firstDiff === secondDiff){
+            (newThumbCurrentPercent < firstThumbPercent)
+                ? (this.view.selectObject = this.view.sliderThumb,
+                    this.changeThumbInModel(newThumbCurrentPercent))
+                : (this.view.selectObject = this.view.sliderThumbSecond!,
+                    this.changeThumbSecondInModel(newThumbCurrentPercent));
         }
         this.view.selectObject = {};
     }
