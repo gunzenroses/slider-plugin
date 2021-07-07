83ec1a6c18683e340616632e7a6324d36b173e51
Karina G <coren.gun@gmail.com>
2021-07-07 17:45:14 +0800

    fix: problem in logic of 'fromValueToPercents' function

--- src/assets/scripts/subview/trackView/sliderRange/sliderRangeView.ts
+++ src/assets/scripts/subview/trackView/sliderRange/sliderRangeView.ts
@@ -1,78 +1,26 @@
 import { changeRange } from "./changeRange"
-import { applyStep } from "../../../common"
+import { applyStepOnPercents } from "../../../common"
 import { TRangeStyle } from "../../../types/types";
 
-function sliderRangeView(parentNode: HTMLElement, ifRange: boolean, ifHorizontal: boolean, max: number, min: number, step: number){
-    let sliderRangeClass: string = ifRange
-                            ? (ifHorizontal ? "slider__range-true"
-                                            : "slider__range_vertical-true")
-                            : (ifHorizontal ? "slider__range"
-                                            : "slider__range_vertical")
+function sliderRangeView(parentNode: HTMLElement, ifRange: boolean, ifHorizontal: boolean, currentFirst: number, currentSecond: number){
+    //here values are in %
+    let sliderRangeClass: string = ifHorizontal
+                            ? "slider__range"
+                            : "slider__range_vertical";
     let sliderRange = document.createElement("div");
     sliderRange.classList.add(`${sliderRangeClass}`);
 
-    let rangeRow: TRangeStyle[] = [];
-    switch (String(sliderRangeClass)){
-        case "slider__range": rangeRow = [
-            {
-                newThumbCurrent: 0, 
-                ifHorizontal: true, 
-                ifRange: true, 
-                ifThumbFirst: true
-            }, 
-            {
-                newThumbCurrent: 66, 
-                ifHorizontal: true, 
-                ifRange: true, 
-                ifThumbFirst: false}
-            ]; 
+    switch(ifRange){
+        case true: 
+            changeRange(sliderRange, currentFirst, ifHorizontal, ifRange, true)
+            changeRange(sliderRange, currentSecond, ifHorizontal, ifRange, false)
             break;
-        case "slider__range-true": rangeRow = [
-            {
-                newThumbCurrent: 33, 
-                ifHorizontal: true, 
-                ifRange: true, 
-                ifThumbFirst: true
-            }, 
-            {
-                newThumbCurrent: 66, 
-                ifHorizontal: true, 
-                ifRange: true, 
-                ifThumbFirst: false}
-            ]; 
+        case false:
+            changeRange(sliderRange, currentFirst, ifHorizontal, ifRange, true);
             break;
-        case "slider__range_vertical": rangeRow = [
-            {
-                newThumbCurrent: 0, 
-                ifHorizontal: false, 
-                ifRange: true, 
-                ifThumbFirst: true
-            }, 
-            {
-                newThumbCurrent: 66, 
-                ifHorizontal: false, 
-                ifRange: true, 
-                ifThumbFirst: false}
-            ]; 
-            break;
-        case "slider__range_vertical-true": rangeRow = [
-            {
-                newThumbCurrent: 33, 
-                ifHorizontal: false, 
-                ifRange: true, 
-                ifThumbFirst: true
-            }, 
-            {
-                newThumbCurrent: 66, 
-                ifHorizontal: false, 
-                ifRange: true, 
-                ifThumbFirst: false}
-            ]; 
-            break;
+        default: break;
     }
 
-    rangeRow.forEach(item => changeRange(sliderRange, applyStep(item.newThumbCurrent, max, min, step), item.ifHorizontal, item.ifRange, item.ifThumbFirst))
-    
     parentNode.append(sliderRange);
     return sliderRange;
 }
