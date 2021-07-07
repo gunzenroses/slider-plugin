0eb8197507d3fc0eaf4bae2407dec1c55126651d
Karina G <coren.gun@gmail.com>
2021-07-08 00:08:44 +0800

    rename: 'actualValue' -> 'valueInPercents' cause it's semantically correct

--- src/assets/scripts/subview/tooltipView/tooltipItemView.ts
+++ src/assets/scripts/subview/tooltipView/tooltipItemView.ts
@@ -1,12 +1,12 @@
 import { changeTooltip } from "./changeTooltip";
 
-function tooltipItemView(parentNode: HTMLElement, className: string, actualValue: number, ifHorizontal: boolean, maxValue: number, minValue: number): HTMLElement {
+function tooltipItemView(parentNode: HTMLElement, className: string, valueInPercents: number, ifHorizontal: boolean, maxValue: number, minValue: number): HTMLElement {
     let verticalClass = ifHorizontal ? "tooltip_horizontal" : "tooltip_vertical";
     let tooltip = document.createElement("span");
     tooltip.classList.add(className, verticalClass);
     tooltip.dataset.name = "tooltip";
 
-    changeTooltip(tooltip, actualValue, maxValue, minValue);
+    changeTooltip(tooltip, valueInPercents, maxValue, minValue);
     console.log(tooltip.innerText)
     parentNode.append(tooltip);
     return tooltip;
