32746a26ca5837fa337bb861b8654d02466e41b6
Karina G <coren.gun@gmail.com>
2021-07-07 19:59:12 +0800

    feat: simplification of tooltipItemView: - takes less params: ('max', 'min') -> ('valuePerPercent'); - replace function 'fromPercentsToValue' with calculations (valueInPercents * valuePerPercent).

--- src/assets/scripts/subview/tooltipView/tooltipItemView.ts
+++ src/assets/scripts/subview/tooltipView/tooltipItemView.ts
@@ -1,16 +1,11 @@
-import { applyStep, fromPercentsToValue } from "../../common"
+import { applyStepOnPercents, fromPercentsToValue } from "../../common"
 
-function tooltipItemView(parentNode: HTMLElement, className: string, value: number, ifHorizontal: boolean, max: number, min: number, step: number): HTMLElement {
+function tooltipItemView(parentNode: HTMLElement, className: string, valueInPercents: number, ifHorizontal: boolean, valuePerPercent: number): HTMLElement {
     let verticalClass = ifHorizontal ? "" : "-vertical";
     let tooltip = document.createElement("span");
     tooltip.classList.add("tooltip", `${className}${verticalClass}`);
     tooltip.dataset.name = "tooltip";
-    let newValue = Math.round(applyStep(value, max, min, step)) > max
-                    ? max
-                    : Math.round(applyStep(value, max, min, step));
-
-    tooltip.innerText = fromPercentsToValue(newValue, max, min);
-    
+    tooltip.innerText = Math.round(valueInPercents * valuePerPercent).toString();
     parentNode.append(tooltip);
     return tooltip;
 }
