bf4b57004934de28c5215500bfeb0feda4ebb0c6
Karina G <coren.gun@gmail.com>
2021-07-11 17:36:49 +0800

    add: adjust data methods in 'setData'

--- src/assets/scripts/model.ts
+++ src/assets/scripts/model.ts
@@ -1,6 +1,7 @@
 import { TSettings } from "./types/types"
 import { EventDispatcher } from './eventDispatcher'
 import { applyStepOnValue, mergeData } from "./common"
+import { adjustCurrentFirst, adjustCurrentSecond, adjustMax, adjustMin, adjustStep } from "./panel/adjustPanel";
 
 interface IModel {
     fromModelChangeView: EventDispatcher;
@@ -37,12 +38,23 @@ class SliderModel implements IModel {
     setData(name: string, data: any){
         let oldData = this.getData();
         if ((oldData[name]) === data) return;
+        switch (name) {
+            case "step": data = adjustStep(data, this.data.max, this.data.min); break;
+            case "min": data = adjustMin(data, this.data.max, this.data.step); break;
+            case "max": data = adjustMax(data, this.data.min, this.data.step); break;
+            case "currentFirst": data = adjustCurrentFirst(data, this.data.currentSecond, this.data.max, this.data.min, this.data.step); break;
+            case "currentSecond": data = adjustCurrentSecond(data, this.data.currentFirst, this.data.max, this.data.min, this.data.step); break;
+            default: break;
+        }
         let newData = { [name]: data };
         this.data = mergeData(oldData, newData);
         this.updateCurrentsWithStep();
         this.fromModelUpdateData.notify(); //Object.keys(newData)[0], Object.values(newData)[0]);
     }
 
+    //here is a mistake! 
+    //when max=11, min 10, 
+    // cf = 30, cs = 140!!
     updateCurrentsWithStep(){
         this.data.currentFirst = applyStepOnValue(this.data.currentFirst, this.data.max, this.data.min, this.data.step)
         this.data.currentSecond =  (this.data.range)
