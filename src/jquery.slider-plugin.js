030e075c650fbbdf30056347d5ba59adac305b8c
Karina G <coren.gun@gmail.com>
2021-08-18 20:49:35 +0800

    add: to sliderPlugin parameters: panel

--- src/jquery.slider-plugin.js
+++ src/jquery.slider-plugin.js
@@ -1,6 +1,6 @@
-import sliderMaker from "./components/mvp/sliderMaker.ts";
+import SliderMaker from "./components/mvp/sliderMaker.ts";
 import "assets/slider.scss";
 
-;($.fn.sliderPlugin = function(options) {
-  sliderMaker(this.get(0), options);
+;($.fn.sliderPlugin = function(options, panel) {
+  return new SliderMaker(this.get(0), options, panel);
 });
