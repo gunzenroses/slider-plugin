29dbfc45b49a215376ede89ced3aea1ea5e6b894
Karina G <coren.gun@gmail.com>
2021-08-18 19:41:02 +0800

    replace: func 'sliderMaker' -> 'jquery.slider-plugin'

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import SliderMaker from "./components/mvp/sliderMaker.ts";
+import sliderPlugin from "./jquery.slider-plugin";
 import "./index.css";
 
 window.onload = function () {
-  let simpleSlider = SliderMaker("horizontal", {
-    min: 10,
-    max: 200,
-    step: 120,
-    scale: true,
-    range: false
-  }, true)
+  $("#horizontal").sliderPlugin({
+      min: 10,
+      max: 200,
+      step: 120,
+      scale: true,
+      range: false
+    }, true)
 
-  let verticalSlider = SliderMaker("vertical", {
+  $("#vertical").sliderPlugin({
     orientation: "vertical",
     step: 6,
     max: 140,
@@ -19,5 +19,5 @@ window.onload = function () {
     },
     range: true,
     tooltip: false
-  }, true)
+  });
 }
