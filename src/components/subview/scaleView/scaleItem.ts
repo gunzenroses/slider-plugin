f757625bd0f86b51314dda66c35a4467122583f8
Karina G <coren.gun@gmail.com>
2021-08-23 02:37:35 +0800

    refine: func scaleItem: algorythm of rendering numbers in scale

--- src/components/subview/scaleView/scaleItem.ts
+++ src/components/subview/scaleView/scaleItem.ts
@@ -1,17 +1,42 @@
+import { commonDivider } from "utils/common";
+
 export default function scaleItem(
   item: number,
+  index: number,
   sequenceNum: number,
+  itemWidth: number,
   ifHorizontal: boolean,
   segmentClass: string,
   spanClass: string,
   min: number,
-  step: number
+  max: number,
+  step: number,
+  tailContainer: number
 ) {
-  let itemClass: string = ifHorizontal
+  const itemClass: string = ifHorizontal
     ? "scale__point"
     : "scale__point_vertical";
 
-  return item === min || (item - min) % (sequenceNum * step) === 0
-    ? `<div class=${segmentClass}><span class="${spanClass}">${item}</span></div>`
-    : `<div class=${itemClass}></div>`;
+  let special: string = (item === max) && (tailContainer < 25)
+      ? `style= "visibility: hidden;"`
+      : "";
+
+  if (itemWidth > 40) {
+    return `<div class=${segmentClass}><span class="${spanClass}" ${special}>${item}</span></div>`
+  } else {
+    let temp: number = Math.round(40/itemWidth);
+    let numOfItems: number = (sequenceNum <= temp)
+      ? temp
+      : (sequenceNum % temp === 0)
+        ? temp
+        : commonDivider(sequenceNum, temp);
+
+    return item === min 
+          || 
+            ((item - min) % (sequenceNum * step) === 0) 
+            && (index % numOfItems === 0)
+            && (index % sequenceNum === 0)
+      ? `<div class=${segmentClass}><span class="${spanClass}" ${special}>${item}</span></div>`
+      : `<div class=${itemClass}></div>`;
+  } 
 }
