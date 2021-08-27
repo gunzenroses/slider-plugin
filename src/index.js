763ae21595fb1a9001f8a2511fa4793189cf4f74
Karina G <coren.gun@gmail.com>
2021-08-27 17:34:17 +0800

    index.js: prettify + eslintignore

--- src/index.js
+++ src/index.js
@@ -2,23 +2,26 @@ import sliderPlugin from "./jquery.slider-plugin";
 import "./index.css";
 
 window.onload = function () {
-  $("#horizontal").sliderPlugin({
+  $("#horizontal").sliderPlugin(
+    {
       min: 10,
       max: 200,
       step: 120,
       scale: true,
-      range: false
-    }, true);
+      range: false,
+    },
+    true
+  );
 
   $("#vertical").sliderPlugin({
     orientation: "vertical",
     step: 6,
     max: 140,
     scale: {
-      stepPerDiv: 2
+      stepPerDiv: 2,
     },
     range: true,
-    tooltip: false
+    tooltip: false,
   });
 
   $("#default").sliderPlugin({}, true);
