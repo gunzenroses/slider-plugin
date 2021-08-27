85f257334d8c58884a0e5656226fb22bf20b697e
Karina G <coren.gun@gmail.com>
2021-08-27 17:38:03 +0800

    delete: redundant file

--- src/components/subview/slidreElement.ts
+++
@@ -1,5 +0,0 @@
-export default abstract class SliderElement {
-  abstract init(...options: any): HTMLElement;
-  abstract make(...options: any): HTMLElement;
-  abstract change(...options: any): HTMLElement;
-}
