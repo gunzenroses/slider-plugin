8fc75b678604a54fa96fc08b027c852e7cf492c7
Karina G <coren.gun@gmail.com>
2021-09-02 23:28:07 +0800

    rename: plugin

--- src/slider-plugin.js
+++ src/slider-plugin.js
@@ -1,6 +1,8 @@
 import SliderMaker from "./components/mvp/sliderMaker.ts";
 import "assets/slider.scss";
 
-;($.fn.sliderPlugin = function(options, panel) {
+(function($){
+  $.fn.sliderMaker = function (options, panel) {
   return new SliderMaker(this.get(0), options, panel);
-});
+  };
+})(jQuery);
