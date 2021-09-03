7f72eb0da322207e09c7e28e19c6f470bff94d5c
Karina G <coren.gun@gmail.com>
2021-09-03 14:22:26 +0800

    test: adjustValue.ts: enhance coverage

--- __tests__/scripts/adjustValue.test.ts
+++ __tests__/scripts/adjustValue.test.ts
@@ -1,52 +1,172 @@
 import { sliderData } from "mvp/data";
 import adjustValue from "helpers/adjustValue";
+import { applyStepOnValue } from "utils/common";
 
 const data = sliderData;
 
 describe("adjustValue", () => {
-  test("should adjust and update negative 'step', 'min' and 'max' data in model", () => {
-    const step = adjustValue("step", -2, data);
-    const min = adjustValue("min", -2, data);
-    const max = adjustValue("max", -130, data);
+  describe("should adjust 'min'", () => {
+    test("< 0", () => {
+      const min = adjustValue("min", -2, data);
 
-    expect(step).toBe(1);
-    expect(min).toBe(0);
-    expect(max).toBe(1);
+      expect(min).toBe(0);
+    });
+
+    test("< 'max' - 'step'", () => {
+      const temp = data.max - data.step - 1;
+      const min = adjustValue("min", temp, data);
+
+      expect(min).toBe(temp);
+    });
+
+    test("=== 'max' - 'step'", () => {
+      const temp = data.max - data.step;
+      const min = adjustValue("min", temp, data);
+
+      expect(min).toBe(temp);
+    });
+
+    test("> 'max' - 'step'", () => {
+      const temp = data.max - data.step + 1;
+      const min = adjustValue("min", temp, data);
+
+      expect(min).toBe(1);
+    });
+
+    test("=== NaN", () => {
+      const min = adjustValue("min", NaN, data);
+
+      expect(min).toBe(1);
+    });
   });
 
-  test("should adjust and update 'min' when it's > than (max - step)", () => {
-    const min = adjustValue("min", 103, data);
+  describe("should adjust 'max'", () => {
+    test("< 0", () => {
+      const max = adjustValue("max", -130, data);
 
-    expect(min).toBe(1);
+      expect(max).toBe(data.min + data.step);
+    });
+
+    test("> (step + min) make no changes", () => {
+      const temp = data.min + data.step + 3;
+      const max = adjustValue("max", temp, data);
+
+      expect(max).toBe(temp);
+    });
+
+    test("=== (step + min) make no changes", () => {
+      const temp = data.step + data.min;
+      const max = adjustValue("max", temp, data);
+
+      expect(max).toBe(temp);
+    });
+
+    test("=== NaN", () => {
+      const max = adjustValue("min", " ", data);
+
+      expect(max).toBe(data.min + data.step);
+    });
   });
 
-  test("should adjust and update 'step' > (max - min)", () => {
-    const step = adjustValue("step", 108, data);
+  describe("should adjust 'currentFirst'", () => {
+    test("in a right range make no changes", () => {
+      const currentFirst = adjustValue("currentFirst", 11, data);
 
-    expect(step).toBe(100);
+      expect(currentFirst).toBe(11);
+    });
+
+    test("< 0", () => {
+      const currentFirst = adjustValue("currentFirst", -11, data);
+
+      expect(currentFirst).toBe(0);
+    });
+
+    test("'currentFirst' > 'currentSecond'", () => {
+      const currentSecond = data.currentSecond;
+      const morethanCS = currentSecond + 10;
+      const currentFirst = adjustValue("currentFirst", morethanCS, data);
+
+      expect(currentFirst).toBe(currentSecond);
+    });
+
+    test("=== NaN", () => {
+      const min = data.min;
+      const currentFirst = adjustValue("currentFirst", NaN, data);
+
+      expect(currentFirst).toBe(min);
+    });
   });
 
-  test("should not change 'currentFirst' and 'currentSecond' if they are > 0", () => {
-    const currentFirst = adjustValue("currentFirst", 11, data);
-    const currentSecond = adjustValue("currentSecond", 35, data);
+  describe("should adjust 'currentSecond'", () => {
+    test("in a right range", () => {
+      const val = 35;
+      const currentSecond = adjustValue("currentSecond", val, data);
+      const temp = applyStepOnValue(val, data.max, data.min, data.step);
 
-    expect(currentFirst).toBe(11);
-    expect(currentSecond).toBe(35);
+      expect(currentSecond).toBe(temp);
+    });
+
+    test("< 0", () => {
+      const currentSecond = adjustValue("currentSecond", -35, data);
+
+      expect(currentSecond).toBe(33);
+    });
+
+    test("< 'currentFirst'", () => {
+      const currentFirst = data.currentFirst;
+      const lessThanCF = currentFirst - 10;
+      const currentSecond = adjustValue("currentSecond", lessThanCF, data);
+
+      expect(currentSecond).toBe(currentFirst);
+    });
+
+    test("=== NaN", () => {
+      const temp = data.currentFirst;
+      const currentSecond = adjustValue("currentSecond", NaN, data);
+
+      expect(currentSecond).toBe(temp);
+    });
+
+    test("> 'max'", () => {
+      const max = data.max;
+      const temp = max + 1;
+      const currentSecond = adjustValue("currentSecond", temp, data);
+
+      expect(currentSecond).toBe(max);
+    });
   });
 
-  test("should adjust and update negative 'currentFirst' and 'currentSecond' data in model", () => {
-    const currentFirst = adjustValue("currentFirst", -11, data);
-    const currentSecond = adjustValue("currentSecond", -35, data);
+  describe("should adjust 'step'", () => {
+    test("> (max - min)", () => {
+      const step = adjustValue("step", 108, data);
 
-    expect(currentFirst).toBe(0);
-    expect(currentSecond).toBe(33);
-  });
+      expect(step).toBe(100);
+    });
 
-  test("should adjust and update 'currentFirst' > 'currentSecond'", () => {
-    const currentFirst = adjustValue("currentFirst", 77, data);
-    const currentSecond = adjustValue("currentSecond", 50, data);
+    test("< (max - min)", () => {
+      const temp = data.max - data.min - 1;
+      const step = adjustValue("step", temp, data);
 
-    expect(currentFirst).toBe(65);
-    expect(currentSecond).toBe(50);
+      expect(step).toBe(temp);
+    });
+
+    test("=== (max-min)", () => {
+      const temp = data.max - data.min;
+      const step = adjustValue("step", temp, data);
+
+      expect(step).toBe(temp);
+    });
+
+    test("=== NaN", () => {
+      const step = adjustValue("step", NaN, data);
+
+      expect(step).toBe(1);
+    });
+
+    test("< 0", () => {
+      const step = adjustValue("step", -2, data);
+
+      expect(step).toBe(1);
+    });
   });
 });
