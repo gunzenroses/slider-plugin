e10d963b2e7f9a7ab15f9a8661c31df374a0e472
gunzenroses <coren.gun@gmail.com>
2021-12-25 11:14:27 +0800

    add: work on value of type string in func adjustValue

--- src/scripts/helpers/adjustValue.ts
+++ src/scripts/helpers/adjustValue.ts
@@ -3,75 +3,95 @@ import { applyStepOnValue, getNumbersAfterDot } from "Utils/common";
 
 export default function adjustValue(name: string, value: TModelData, data: TSettings): TModelData {
   const { max, min, step, currentFirst, currentSecond }: TSettings = data;
-  if (typeof value === "number") {
+  if (typeof value === "string" || typeof value === "number") {
     switch (name) {
-    case "step":
-      value = adjustStep(value);
-      break;
-    case "min":
-      value = adjustMin(value);
-      break;
-    case "max":
-      value = adjustMax(value);
-      break;
-    case "currentFirst":
-      value = adjustCurrentFirst(value);
-      break;
-    case "currentSecond":
-      value = adjustCurrentSecond(value);
-      break;
-    default:
-      value = adjustAsIs(value);
-      break;
-    }
+      case "step":
+        value = adjustStep(value);
+        break;
+      case "min":
+        value = adjustMin(value);
+        break;
+      case "max":
+        value = adjustMax(value);
+        break;
+      case "currentFirst":
+        value = adjustCurrentFirst(value);
+        break;
+      case "currentSecond":
+        value = adjustCurrentSecond(value);
+        break;
+      default:
+        value = adjustAsIs(value);
+        break;
+    } 
   } else {
     value = adjustAsIs(value);
   }
   
 
-  function adjustMin(value: number): number {
-    return value <= max - step ? value : 0;
+  function adjustMin(value: number | string): number {
+    if (typeof value === "string") {
+      return 0;
+    } else {
+      return value <= max - step ? value : 0;
+    }
   }
 
-  function adjustMax(value: number): number {
+  function adjustMax(value: number | string): number {
     const decimalMin = getNumbersAfterDot(min);
     const decimalStep = getNumbersAfterDot(step);
     const decimalCommon = Math.max(decimalMin, decimalStep);
     const decimalMultiply = 10**decimalCommon
     const lowestMax = (min * decimalMultiply + step * decimalMultiply)/decimalMultiply;
-    return value >= lowestMax ? value : lowestMax;
+    if (typeof value === "string") {
+      return lowestMax;
+    } else {
+      return value >= lowestMax ? value : lowestMax;
+    }
   }
 
-  function adjustStep(value: number): number {
-    return value <= 1
-      ? 1
-      : value > max - min
-      ? max - min
-      : value < max - min
-      ? value
-      : value === max - min
-      ? max - min
-      : 1;
+  function adjustStep(value: number | string): number {
+    if (typeof value === "string") {
+      return 1; 
+    } else {
+      return value <= 1
+        ? 1
+        : value > max - min
+        ? max - min
+        : value < max - min
+        ? value
+        : value === max - min
+        ? max - min
+        : 1;
+    }
   }
 
-  function adjustCurrentFirst(value: number): number {
-    return value < min
-      ? min
-      : value > currentSecond
-      ? currentSecond
-      : value <= currentSecond
-      ? applyStepOnValue(value, data)
-      : min;
+  function adjustCurrentFirst(value: number | string): number {
+    if (typeof value === "string") {
+      return min; 
+    } else {
+      return value < min
+        ? min
+        : value > currentSecond
+        ? currentSecond
+        : value <= currentSecond
+        ? applyStepOnValue(value, data)
+        : min;
+    }
   }
 
-  function adjustCurrentSecond(value: number): number {
-    return value < currentFirst
-      ? currentFirst
-      : value > max
-      ? max
-      : value <= max
-      ? applyStepOnValue(value, data)
-      : currentFirst;
+  function adjustCurrentSecond(value: number | string): number {
+    if (typeof value === "string") {
+      return max; 
+    } else {
+      return value < currentFirst
+        ? currentFirst
+        : value > max
+        ? max
+        : value <= max
+        ? applyStepOnValue(value, data)
+        : currentFirst;
+    }
   }
 
   function adjustAsIs(value: TModelData): TModelData {
