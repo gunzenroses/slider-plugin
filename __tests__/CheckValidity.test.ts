adfeef146918d4ebe0745a9da9d8391b0af6a092
gunzenroses <coren.gun@gmail.com>
2022-01-28 16:01:16 +0800

    add: CheckValidity.test.ts

---
+++ __tests__/CheckValidity.test.ts
@@ -0,0 +1,80 @@
+/*
+ * @jest-environment jsdom
+ */
+
+import CheckValidity from 'helpers/CheckValidity';
+
+const container = document.createElement('div');
+document.body.append(container);
+
+const item = document.createElement('input');
+item.type = 'number';
+
+describe('helpers: class CheckValidity', () => {
+  test('return false when input.value=""', () => {
+    item.value = '';
+
+    const checkItem = new CheckValidity(item, container);
+
+    expect(checkItem.checkValidity()).toBeFalsy();
+  });
+
+  test('add error-message when value is not a number', () => {
+    item.value = 'cat';
+
+    const checkItem = new CheckValidity(item, container);
+
+    expect(checkItem.invalidities).toContain('Should be a number');
+  });
+
+  test('return nothing when input has a proper value', () => {
+    item.value = '15';
+    item.step = '5';
+    item.min = '0';
+    item.max = '30';
+
+    const checkItem = new CheckValidity(item, container);
+
+    expect(checkItem.invalidities).toEqual([]);
+  });
+
+  test('add error-message when value > item', () => {
+    item.value = '31';
+    item.max = '30';
+
+    const checkItem = new CheckValidity(item, container);
+
+    expect(checkItem.invalidities).toContain('Number should be maximum 30');
+  });
+
+  test('add error-message when stepMismatch and min=0', () => {
+    item.min = '0';
+    item.step = '4';
+    item.value = '11';
+    item.name = 'from';
+
+    const checkItem = new CheckValidity(item, container);
+
+    expect(checkItem.invalidities).toContain('Number should be multiple of 4');
+  });
+
+  test('add error-message when stepMismatch and min>0', () => {
+    item.min = '10';
+    item.step = '8';
+    item.value = '25';
+    item.name = 'from'
+    
+    const checkItem = new CheckValidity(item, container);
+
+    expect(checkItem.invalidities).toContain('Number should be: 10 + multiple of 8');
+  });
+
+  test('add error-message when value < min', () => {
+    item.min = '1';
+    item.value = '0';
+
+    const checkItem = new CheckValidity(item, container);
+
+    expect(checkItem.invalidities).toContain('Number should be minimum 1');
+  });
+});
