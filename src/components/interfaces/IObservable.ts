a9f097de5aa7261a0d8b55364074a40dfb4f33b0
gunzenroses <coren.gun@gmail.com>
2022-01-30 11:34:37 +0800

    delete: wrong named folder

--- src/components/interfaces/IObservable.ts
+++
@@ -1,9 +0,0 @@
-import { TListener, TListenerArg } from 'utils/types';
-
-interface IObservable {
-  add(eventKey: string, listener: TListener): void;
-  notify(eventKey: string, args: TListenerArg): void;
-  notify(eventKey: string): void;
-}
-
-export default IObservable;
