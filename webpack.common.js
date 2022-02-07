c9aad141623b3509c291ecc3fad47ccd958cebdc
gunzenroses <coren.gun@gmail.com>
2022-02-08 00:28:52 +0800

    feat: apply eslint

--- webpack.common.js
+++ webpack.common.js
@@ -3,47 +3,47 @@ const webpack = require("webpack");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  context: path.resolve(__dirname, "./src"),
+  context: path.resolve(__dirname, './src'),
   node: {
     __filename: true,
     __dirname: true,
   },
   optimization: {
-    minimize: true
+    minimize: true,
   },
   resolve: {
     alias: {
-      SliderMaker: path.resolve(__dirname, "src/components/SliderMaker"),
-      Model: path.resolve(__dirname, "src/components/Model"),
-      View: path.resolve(__dirname, "src/components/View"),
-      Presenter: path.resolve(__dirname, "src/components/Presenter"),
-      Panel: path.resolve(__dirname, "src/components/Panel/"),
-      Observable: path.resolve(__dirname, "src/components/Observable/"),
-      Interfaces: path.resolve(__dirname, "src/components/Interfaces/"),
-      assets: path.resolve(__dirname, "src/assets/"),
-      helpers: path.resolve(__dirname, "src/scripts/helpers/"),
-      utils: path.resolve(__dirname, "src/scripts/utils/"),
-      scripts: path.resolve(__dirname, "src/scripts/"),
+      SliderMaker: path.resolve(__dirname, 'src/components/SliderMaker'),
+      Model: path.resolve(__dirname, 'src/components/Model'),
+      View: path.resolve(__dirname, 'src/components/View'),
+      Presenter: path.resolve(__dirname, 'src/components/Presenter'),
+      Panel: path.resolve(__dirname, 'src/components/Panel/'),
+      Observable: path.resolve(__dirname, 'src/components/Observable/'),
+      Interfaces: path.resolve(__dirname, 'src/components/Interfaces/'),
+      assets: path.resolve(__dirname, 'src/assets/'),
+      helpers: path.resolve(__dirname, 'src/scripts/helpers/'),
+      utils: path.resolve(__dirname, 'src/scripts/utils/'),
+      scripts: path.resolve(__dirname, 'src/scripts/'),
     },
-    extensions: [".ts", ".tsx", ".js", ".scss"],
-    modules: ["node_modules", path.resolve(__dirname, "src")],
+    extensions: ['.ts', '.tsx', '.js', '.scss'],
+    modules: ['node_modules', path.resolve(__dirname, 'src')],
   },
   module: {
     rules: [
       {
         test: /\.pug$/,
-        loader: "pug-loader",
-        options: { 
+        loader: 'pug-loader',
+        options: {
           pretty: true,
-        }
+        },
       },
       {
         test: /\.js$/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-          }
+            presets: ['@babel/preset-env'],
+          },
         },
         exclude: /node_modules/,
       },
@@ -57,5 +57,5 @@ module.exports = {
       "window.jQuery": "jquery",
       "window.$": "jquery",
     }),
-  ]
+  ],
 };
