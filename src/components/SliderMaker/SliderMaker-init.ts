bcf1f539fa949f242c864e0fc01842afad7972e0
gunzenroses <coren.gun@gmail.com>
2022-02-22 23:59:45 +0800

    #5: make default selector to initialize slider-plugin

--- src/components/SliderMaker/SliderMaker-init.ts
+++ src/components/SliderMaker/SliderMaker-init.ts
@@ -1,12 +1,17 @@
 import SliderMaker from './SliderMaker';
 
 $.fn.sliderMaker = function makeSlider(
-  options: TSettings,
-  ifPanel: boolean
-): JQuery {
+  options?: TSettings,
+  ifPanel?: boolean
+): SliderMaker | undefined {
   const element = this.get(0);
-  if (element !== undefined) {
-    new SliderMaker(element, options, ifPanel);
+  if (element) {
+    return new SliderMaker(ifPanel, element, options);
   }
-  return this;
+  else {
+    document.querySelectorAll('.js-slider-init').forEach((slider) => {
+      return new SliderMaker(ifPanel, slider as HTMLElement, options);
+    })
+  }
+  return undefined;
 };
