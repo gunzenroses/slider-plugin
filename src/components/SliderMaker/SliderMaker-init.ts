b0784ffcbfe30767f02f1fbd7afe3fac1796aeca
gunzenroses <coren.gun@gmail.com>
2022-02-23 15:10:24 +0800

    #5: make '.js-slider-init' sufficient for slider initialization

--- src/components/SliderMaker/SliderMaker-init.ts
+++ src/components/SliderMaker/SliderMaker-init.ts
@@ -1,17 +1,23 @@
 import SliderMaker from './SliderMaker';
 
+$(document).ready(() => {
+  $('.js-slider-init').each(
+    (_, $slider) => {
+      if ($slider.children.length < 1) {
+        new SliderMaker($slider);
+      }
+    }
+  );
+});
+
 $.fn.sliderMaker = function makeSlider(
   options?: TSettings,
   ifPanel?: boolean
 ): SliderMaker | undefined {
   const element = this.get(0);
   if (element) {
-    return new SliderMaker(ifPanel, element, options);
+    return new SliderMaker(element, ifPanel, options);
   }
-  else {
-    document.querySelectorAll('.js-slider-init').forEach((slider) => {
-      return new SliderMaker(ifPanel, slider as HTMLElement, options);
-    })
-  }
+
   return undefined;
 };
